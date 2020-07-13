79d960eae6926f398230f47031b66451f767e210
Goutemberg Pessoa <goutembergpessoa@Goutembergs-MacBook-Air.local>
2020-07-13 19:50:34 -0300

    Subida do ProjetoBlog

---
+++ posts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+from posts.models import Post
+
+class PostIndex(ListView):
+    model = Post
+    template_name = 'posts/index.html'
+    paginate_by = 3
+
+class PostBusca(PostIndex):
+    pass
+
+class PostCategoria(PostIndex):
+    pass
+
+class PostDetalhes(UpdateView):
+    pass
+
