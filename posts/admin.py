98949d8ae3a556c587b53df7b054586308f83ec2
Goutemberg Pessoa <goutembergpessoa@Goutembergs-MacBook-Air.local>
2020-07-13 20:11:08 -0300

    Alteracao do arquivo admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -2,7 +2,6 @@
 from django.contrib import admin
 from .models import Post
 
-
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id','titulo_post', 'autor_post', 'data_post',
                     'categoria_post', 'publicado_post',)
