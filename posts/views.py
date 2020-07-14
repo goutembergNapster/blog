fb7fd351e14b26a676dacfe57e9454669edcab55
Goutemberg Pessoa <goutembergpessoa@Goutembergs-MacBook-Air.local>
2020-07-14 08:47:50 -0300

    Reorganizacao de Documentos

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
-from posts.models import Post
+from .models import Post
 
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
     paginate_by = 3
+    context_object_name = 'posts'
 
 class PostBusca(PostIndex):
     pass
@@ -15,5 +16,8 @@ class PostCategoria(PostIndex):
     pass
 
 class PostDetalhes(UpdateView):
-    pass
+    template_name = 'posts/post_detalhes.html'
+    model = Post
+    context_object_name = 'post'
+
 
