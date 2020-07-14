84bf226694627958a3a13ad4e1f9dbe1fbc884ee
Goutemberg Pessoa <goutembergpessoa@Goutembergs-MacBook-Air.local>
2020-07-14 09:35:56 -0300

    Reestruturacao do Arquivos e Pastas

--- categorias/models.py
+++ categorias/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     nome_cat = models.CharField(max_length=50)
+
     def __str__(self):
         return self.nome_cat
-
-
-#serve para deixar o nome corretamente, tira o nome object
-
